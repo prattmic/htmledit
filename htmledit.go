1a108f840c65d27f3136650b51420e60a10cbe03
Michael Pratt <michael@pratt.im>
2018-04-28 17:53:19 -0700

    Workaround for extra newline added at the end of <body>

--- htmledit.go
+++ htmledit.go
@@ -5,7 +5,41 @@ import (
 	"os"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
+
+func fixBody(d *goquery.Document) {
+	// For some reason, the parser adds an extra newline to the text just
+	// before </body>. Drop it.
+	s := d.Find("body")
+	if s.Length() == 0 {
+		fmt.Fprintf(os.Stderr, "body not found\n")
+		return
+	}
+
+	n := s.Get(0).LastChild
+	if n == nil {
+		fmt.Fprintf(os.Stderr, "body has no children\n")
+		return
+	}
+
+	if n.Type != html.TextNode {
+		fmt.Fprintf(os.Stderr, "body last child %+v not text\n", n)
+		return
+	}
+
+	if len(n.Data) < 1 {
+		fmt.Fprintf(os.Stderr, "body last child has no data\n")
+		return
+	}
+
+	if n.Data[len(n.Data)-1] != '\n' {
+		fmt.Fprintf(os.Stderr, "body last child does not end in newline: %q\n", n.Data)
+		return
+	}
+
+	n.Data = n.Data[:len(n.Data)-1]
+}
 
 func main() {
 	var selector string
@@ -23,6 +57,8 @@ func main() {
 		fmt.Fprintf(os.Stderr, "error parsing file: %v\n", err)
 		os.Exit(1)
 	}
+
+	fixBody(d)
 
 	if selector != "" {
 		s := d.Find(selector)
