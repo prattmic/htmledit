ffb17218d0c92efa14c10ea6b4b269f834116f1d
Michael Pratt <michael@pratt.im>
2018-04-28 17:00:25 -0700

    Remove passed selector

--- htmledit.go
+++ htmledit.go
@@ -8,26 +8,34 @@ import (
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		fmt.Printf("usage: %s file.html\n", os.Args[0])
+	var selector string
+	switch len(os.Args) {
+	case 1:
+	case 2:
+		selector = os.Args[1]
+	default:
+		fmt.Fprintf(os.Stderr, "usage: %s [selector]\n", os.Args[0])
 		os.Exit(1)
 	}
 
-	f, err := os.Open(os.Args[1])
+	d, err := goquery.NewDocumentFromReader(os.Stdin)
 	if err != nil {
-		fmt.Printf("error opening file: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error parsing file: %v\n", err)
 		os.Exit(1)
 	}
 
-	d, err := goquery.NewDocumentFromReader(f)
-	if err != nil {
-		fmt.Printf("error parsing file: %v\n", err)
-		os.Exit(1)
+	if selector != "" {
+		s := d.Find(selector)
+		if s.Length() == 0 {
+			fmt.Fprintf(os.Stderr, "selector %q not found\n", selector)
+			os.Exit(1)
+		}
+		s.Remove()
 	}
 
 	h, err := goquery.OuterHtml(d.Selection)
 	if err != nil {
-		fmt.Printf("error generating html: %v\n", err)
+		fmt.Fprintf(os.Stderr, "error generating html: %v\n", err)
 		os.Exit(1)
 	}
 
